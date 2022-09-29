a978a1ee5a86a985cc4e94c953b7b9710dec82ba
JFerrettUK <jferrettuk@gmail.com>
2022-09-29 10:34:59 +0100

    TTT Javascript module and function updates

--- javascript.js
+++ javascript.js
@@ -1,40 +1,43 @@
-const gameState = () => {
-    const gameboard = (() => {
+// const gameboard = (() => {
 
-        const gameboard = []
+//     const gameboardArray = ["X", "", "O", "", "", "", "", "", ""]
 
-        function displayGameboard(gameboard) {
-            for (var i = 0; i < gameboard.length; i++) {
-              document.getElementById(`box${i}`).textContent = gameboard[i];
-            }
-        }
+//     function displayGameboard(gameboardArray) {
+//         for (var i = 0; i < gameboardArray.length; i++) {
+//           document.getElementById(`box${i}`).textContent = gameboardArray[i];
+//         }
+//     }
 
-        displayGameboard(gameboard);
+//     displayGameboard(gameboardArray);
 
-        return { 
-          gameboard
-        };
+//     return { 
+//       gameboardArray
+//     };
+
+// })();
+
+const box1 = "test";
+const box2 = "test";
+
+const gameboard = (() => {
+  const gameboardArray = (val1, val2) => [val1, val2]
+  return {
+    gameboardArray
+  }
+})();
+
+const newPlayer = () => {
+  const player = []
+  return { player };
+};
   
-    })();
-
-    const newPlayer = () => {
-      const player = []
-      return { player };
-    };
-      
-    const player1 = newPlayer();
-    const player2 = newPlayer();
-
-    return { gameboard, player1, player2 };
-
-}
-
-gameState();
+const player1 = newPlayer();
+const player2 = newPlayer();
 
 // If turnOrder is an even number, it's player 1's turn. If it's odd, it's player 2's turn.
 
 let turnOrder = 0;
-
+const gameboardArrayBase = ["", "", "", "", "", "", "", "", ""]
 
 function playerMove(i) { 
 
@@ -45,11 +48,13 @@ function playerMove(i) {
   function playerTurn(i) {
     if (document.getElementById(`box${i}`).textContent == "" && isEven(turnOrder) == true) {
       document.getElementById(`box${i}`).textContent = "X";
+      gameboardArrayBase[`${i}`] = "X"
       document.getElementById(`gameText`).textContent = "Player 2: Go!";
       turnOrder++
     } else if (document.getElementById(`box${i}`).textContent == "") {
       document.getElementById(`box${i}`).textContent = "O";
       document.getElementById(`gameText`).textContent = "Player 1: Go!";
+      gameboardArrayBase[`${i}`] = "O"
       turnOrder++
     }
   }
